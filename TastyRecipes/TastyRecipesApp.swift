d956b96193fa39109703993f966f62bbb8467f8d
kejlor <woj450@gmail.com>
2022-06-12 20:07:19 +0200

    Initial Commit

---
+++ TastyRecipes/TastyRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TastyRecipesApp.swift
+//  TastyRecipes
+//
+//  Created by Bartosz Wojtkowiak on 12/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct TastyRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
